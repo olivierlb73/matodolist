452322541281e4f4c27f8698943feb15c24f5675
olivierlb73 <olivier.leborgne7@gmail.com>
2017-12-02 13:06:21 +0100

    Ajout de twig

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
+
+        res.render('index.twig', {'titre': 'Ma liste de tâches'});    
+
+});
+
+app.use(function(req, res, next){
     
-        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+        res.status(404).render('404.twig');
     
-        res.end('Vous êtes à l\'accueil');
-    
-    });
+});
+
 app.listen(3000);
